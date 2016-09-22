75ae28077c6e8a17fcee5c9b04d77c3e8820b3db
stbui <w431106@163.com>
2016-09-22 13:23:00 +0800

    增加下载功能

--- src/home/controller/index.js
+++ src/home/controller/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import Base from './base.js';
+import fs from 'fs';
 
 
 export default class extends Base {
@@ -37,5 +38,71 @@ export default class extends Base {
         }
 
         this.redirect('/index.html');
+    }
+
+
+    async downloadAction() {
+        const {type} = this.get();
+        const linksModel = await this.model('links').field('title,link').select();
+
+        const folder = think.ROOT_PATH + '/back';
+        const filename = 'favourite';
+        let path = path = folder + '/' + filename + '.json';
+
+        let data = linksModel;
+
+        if (!fs.existsSync(folder)) {
+            fs.mkdirSync(folder);
+        }
+
+
+        switch (type) {
+            case 'html':
+                data = this.htmlFile(data);
+                path = folder + '/' + filename + '.html';
+                this.download(path, filename + '.html');
+                break;
+            case 'markdown':
+                data = this.markdownFile(data);
+                path = folder + '/' + filename + '.md';
+                this.download(path, filename + '.md');
+                fs.writeFileSync(think.ROOT_PATH+'/README.md', data, 'utf-8');
+                break;
+            case 'json':
+                data = JSON.stringify(data);
+                path = folder + '/' + filename + '.json';
+                this.download(path, filename + '.json');
+                break;
+            default:
+                data = JSON.stringify(data);
+                path = folder + '/' + filename + '.json';
+                this.download(path, filename + '.json');
+                break;
+        }
+
+        fs.writeFileSync(path, data, 'utf-8');
+    }
+
+    htmlFile(data) {
+        let str = ``;
+
+        data.forEach(value=> {
+            str += `${value.title} : <a href="${value.link}" target="_blank">${value.link}</a>\r\n`;
+        });
+
+        return str;
+    }
+
+    markdownFile(data) {
+        let str = `
+网址名称 | 网址链接
+:----:|:----:
+`;
+
+        data.forEach(value=> {
+            str += `${value.title} | ${value.link}\r\n`;
+        });
+
+        return str;
     }
 }
